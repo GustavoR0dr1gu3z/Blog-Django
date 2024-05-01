7355cdffedae8d97b61fdb7df5f59b1491b1f7d3
Gustavo Calzada <tym.latam1@gmail.com>
2024-05-01 10:48:35 -0600

    add templates

--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic.base import View
+from django.shortcuts import render
 
 class bienvenidaAdmin(View):
-    def get(self, response):
-        return HttpResponse(
-            content="Hola soy gustavo"
-        )
+    def get(self, request):
+        data = {
+            'name': 'Calzada',
+            'year': 24,
+            'codes': ['Python', 'Django', 'Lit']
+        }
+        return render(request, 'msjBienvenida.html', context=data)
